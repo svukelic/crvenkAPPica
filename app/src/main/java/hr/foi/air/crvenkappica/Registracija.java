c2c4b7ac928b9f3a6d76248dd2c53e8b840436bb
Mojsa <mmojsino@foi.hr>
2015-10-17 12:56:35 +0200

    Activity_registration

---
+++ app/src/main/java/hr/foi/air/crvenkappica/Registracija.java
@@ -0,0 +1,23 @@
+package hr.foi.air.crvenkappica;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class Registracija extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registracija);
+        //ovdje je listener za button
+        Button b1 = (Button) findViewById(R.id.button_reg);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
