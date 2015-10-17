49503011a8b11f85948eb46159f03ac3165e0ee5
domagojk1 <domagoj15_@net.hr>
2015-10-17 15:43:49 +0200

    Izmjene: - Dodan Login Activity

--- app/src/main/java/hr/foi/air/crvenkappica/Login.java
+++ app/src/main/java/hr/foi/air/crvenkappica/Login.java
@@ -1,22 +1,22 @@
 package hr.foi.air.crvenkappica;
 
 import android.app.Activity;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class Login extends AppCompatActivity implements View.OnClickListener {
+import org.w3c.dom.Text;
 
-    EditText etUsername,etPassword;
+public class Login extends Activity implements View.OnClickListener {
+
     Button btnLogin;
-    TextView registerLink;
+    EditText etUsername,etPassword;
+    TextView register;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,22 +25,20 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
         etUsername = (EditText) findViewById(R.id.etUsername);
         etPassword = (EditText) findViewById(R.id.etPassword);
         btnLogin = (Button) findViewById(R.id.btnLogin);
-        registerLink = (TextView) findViewById(R.id.tvRegister);
-
+        register = (TextView) findViewById(R.id.tvRegister);
         btnLogin.setOnClickListener(this);
-        registerLink.setOnClickListener(this);
+        register.setOnClickListener(this);
     }
 
-    public void OnClick(View v){
+    @Override
+    public void onClick(View v) {
         switch (v.getId()){
             case R.id.btnLogin:
 
                 break;
-
             case R.id.tvRegister:
-
-                break;
-
+                Intent intent = new Intent(Login.this,Registracija.class);
+                startActivity(intent);
         }
     }
 }
