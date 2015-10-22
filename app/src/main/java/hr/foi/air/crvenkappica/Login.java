437c31097998a355cfe63a91e967c9b0a829e9b0
domagojk1 <domagoj15_@net.hr>
2015-10-22 14:15:10 +0200

    Male izmjene

--- app/src/main/java/hr/foi/air/crvenkappica/Login.java
+++ app/src/main/java/hr/foi/air/crvenkappica/Login.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,7 +12,7 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
-public class Login extends Activity implements View.OnClickListener {
+public class Login extends AppCompatActivity implements View.OnClickListener {
 
     Button btnLogin;
     EditText etUsername,etPassword;
@@ -40,5 +41,10 @@ public class Login extends Activity implements View.OnClickListener {
                 Intent intent = new Intent(Login.this,Registracija.class);
                 startActivity(intent);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
     }
 }
--- app/src/main/java/hr/foi/air/crvenkappica/MainActivity.java
+++ app/src/main/java/hr/foi/air/crvenkappica/MainActivity.java
@@ -1,5 +1,6 @@
 package hr.foi.air.crvenkappica;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,6 +10,8 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
+
+import java.util.jar.Manifest;
 
 import hr.foi.air.crvenkappica.web.WebParams;
 import hr.foi.air.crvenkappica.web.WebRequest;
@@ -22,41 +25,13 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        Intent i = new Intent(MainActivity.this,Login.class);
+        startActivity(i);
+
         WebParams webParamsInit = new WebParams();
         webParamsInit.service = "con_init.php";
         webParamsInit.hash = "";
         webParamsInit.jsonParams = "";
         new WebRequest().execute(webParamsInit);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
