1a2fa073c51163e5477c36c20c28b75e293631ff
Mojsa <mmojsino@foi.hr>
2015-10-23 20:23:39 +0200

    Klasa za podatke registracije, kreiran json objekt iz tih podataka.

--- app/src/main/java/hr/foi/air/crvenkappica/Registracija.java
+++ app/src/main/java/hr/foi/air/crvenkappica/Registracija.java
@@ -1,6 +1,7 @@
 package hr.foi.air.crvenkappica;
 
 import android.app.DatePickerDialog;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,36 +11,56 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
 import hr.foi.air.crvenkappica.web.WebParams;
 import hr.foi.air.crvenkappica.web.WebRequest;
+import hr.foi.air.crvenkappica.Registration_Data;
 
 public class Registracija extends AppCompatActivity implements View.OnClickListener {
-    private EditText DOB_EditText;
+    private EditText DOB_EditText, User, Pass, Email, Name, Lastname;
     private DatePickerDialog DOB_Picker;
     private SimpleDateFormat dateFormatter;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registracija);
         dateFormatter = new SimpleDateFormat("dd-MM-yyyy", Locale.GERMAN);
         findViewsById();
         setDateTimeField();
-        //ovdje je listener za button
-        WebParams webParamsInit = new WebParams();
-        webParamsInit.service = "con_init.php";
-        webParamsInit.hash = "";
-        webParamsInit.type = "";
-        webParamsInit.params = "";
-        new WebRequest().execute(webParamsInit);
         Button b1 = (Button) findViewById(R.id.button_reg);
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Registration_Data data = new Registration_Data();
+                data.setName(Name.getText().toString());
+                data.setLastname(Lastname.getText().toString());
+                data.setUsername(User.getText().toString());
+                data.setPassword(Pass.getText().toString());
+                data.setEmail(Email.getText().toString());
+                data.setDOB(DOB_EditText.getText().toString());
+                GsonBuilder builder = new GsonBuilder();
+                Gson gson = builder.create();
+                ProgressDialog dialog = new ProgressDialog(Registracija.this);
+                dialog.setTitle(R.string.title_activity_activity__registration);
+                dialog.setMessage("Registration in progress"); //treba provjeriti da se iz strings.xml ucitava
+                dialog.setIndeterminate(false);
+                dialog.setCancelable(true);
+                dialog.show();
+                //System.out.println(gson.toJson(data));
+                WebParams webParamsInit = new WebParams();
+                webParamsInit.service = "con_init.php";
+                webParamsInit.hash = "";
+                webParamsInit.type = "";
+                webParamsInit.params = gson.toJson(data);
+                new WebRequest().execute(webParamsInit);
             }
         });
         DOB_EditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
@@ -54,6 +75,11 @@ public class Registracija extends AppCompatActivity implements View.OnClickListe
     private void findViewsById() {
         DOB_EditText = (EditText) findViewById(R.id.dob_editText);
         DOB_EditText.setInputType(InputType.TYPE_NULL);
+        Name = (EditText) findViewById(R.id.name_editText);
+        Lastname = (EditText) findViewById(R.id.surname_editText);
+        User = (EditText) findViewById(R.id.username_editText);
+        Pass = (EditText) findViewById(R.id.pass_editText);
+        Email = (EditText) findViewById(R.id.email_editText);
         //DOB_EditText.requestFocus();
     }
 
---
+++ app/src/main/java/hr/foi/air/crvenkappica/Registration_Data.java
@@ -0,0 +1,64 @@
+package hr.foi.air.crvenkappica;
+
+import java.util.Date;
+
+
+public class Registration_Data {
+    private String Name;
+    private String Lastname;
+    private String Username;
+    private String Password;
+    private String DOB;
+    private String Email;
+
+    public Registration_Data() {
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getLastname() {
+        return Lastname;
+    }
+
+    public void setLastname(String lastname) {
+        Lastname = lastname;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String dob) {
+        DOB = dob;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+}
