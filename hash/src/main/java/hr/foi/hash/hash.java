a1499226ec23729e43c78bb4d805fb1fb55c55fb
ddrazeno <ddrazeno@foi.hr>
2015-10-26 19:23:49 +0100

    sha256 hash klasa

---
+++ hash/src/main/java/hr/foi/hash/hash.java
@@ -0,0 +1,26 @@
+package hr.foi.hash;
+
+import java.security.MessageDigest;
+
+/**
+ * Created by DOMAGOJ on 26-Oct-15.
+ */
+public class hash {
+    public static String sha256(String base) {
+        try{
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(base.getBytes("UTF-8"));
+            StringBuffer hexString = new StringBuffer();
+
+            for (int i = 0; i < hash.length; i++) {
+                String hex = Integer.toHexString(0xff & hash[i]);
+                if(hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+
+            return hexString.toString();
+        } catch(Exception ex){
+            throw new RuntimeException(ex);
+        }
+    }
+}
