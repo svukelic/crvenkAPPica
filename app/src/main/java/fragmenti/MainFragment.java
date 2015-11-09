8e0a9e010fc5a5ae8f03a679ee731a135e08169f
ddrazeno <ddrazeno@foi.hr>
2015-11-09 21:26:45 +0100

    Navigacija sa fragmentima2

---
+++ app/src/main/java/fragmenti/MainFragment.java
@@ -0,0 +1,25 @@
+package fragmenti;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import hr.foi.air.crvenkappica.R;
+
+/**
+ * Created by DOMAGOJ on 07-Nov-15.
+ */
+public class MainFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container,false);
+
+        return rootView;
+    }
+}
