546d4effeaf58c506635e0a31745d7774f5a4c62
svukelic <svukelic@foi.hr>
2015-12-17 17:06:58 +0100

    Osobni profil, inicijalna verzija

--- app/src/main/java/hr/foi/air/crvenkappica/TestFragment.java
+++ app/src/main/java/hr/foi/air/crvenkappica/TestFragment.java
@@ -1,21 +1,81 @@
 package hr.foi.air.crvenkappica;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
-/**
- * Created by domagoj on 04.12.15..
- */
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import hr.foi.air.crvenkappica.web.AsyncResponse;
+import hr.foi.air.crvenkappica.web.WebParams;
+import hr.foi.air.crvenkappica.web.WebRequest;
+
 public class TestFragment extends Fragment {
+
+    private TextView tvUsername;
+    private TextView tvIme;
+    private TextView tvPrezime;
+    private TextView tvDob;
+    private ProgressDialog progressdialog;
+    private String userName;
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_navigacija,container,false);
+
+
+        tvUsername = (TextView) view.findViewById(R.id.tvUsername);
+        tvUsername.setText("Username: " + LoginStatus.LoginInfo.getLoginName());
+
+        tvIme = (TextView) view.findViewById(R.id.tvIme);
+        tvPrezime = (TextView) view.findViewById(R.id.tvPrezime);
+        tvDob = (TextView) view.findViewById(R.id.tvDOB);
+
+        progressdialog = new ProgressDialog(getActivity());
+        progressdialog.setTitle(R.string.title_activity_login);
+        progressdialog.setMessage("Loading profile");
+        progressdialog.setIndeterminate(false);
+        progressdialog.setCancelable(false);
+        progressdialog.show();
+
+        userName = LoginStatus.LoginInfo.getLoginName();
+
+        if (!userName.isEmpty()) {
+            String hash = "";
+            String type = "";
+            WebParams paramsProfil = new WebParams();
+            paramsProfil.params = "?UserName=" + userName;
+            paramsProfil.service = "profil_dohvat.php";
+            paramsProfil.listener = response;
+
+            new WebRequest().execute(paramsProfil);
+        }
+
         return view;
     }
+
+    AsyncResponse response = new AsyncResponse() {
+        @Override
+        public void processFinish(String output) {
+            //System.out.println(output);
+            progressdialog.hide();
+            Toast.makeText(getActivity(), output, Toast.LENGTH_LONG).show();
+            try {
+                JSONObject jsonObject = new JSONObject(output);
+                tvIme.setText("Ime: " + jsonObject.getString("Ime"));
+            } catch (JSONException e) {
+                Toast.makeText(getActivity(), "Error", Toast.LENGTH_LONG).show();
+            }
+
+        }
+    };
 }
