e2a2245902b15546d42fb534c637a8391d974076
ddrazeno <ddrazeno@foi.hr>
2015-12-18 22:15:51 +0100

    slike_upload

--- app/src/main/java/hr/foi/air/crvenkappica/MainActivity.java
+++ app/src/main/java/hr/foi/air/crvenkappica/MainActivity.java
@@ -1,18 +1,36 @@
 package hr.foi.air.crvenkappica;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ImageView SlikaUpload;
+    EditText Uploadime;
+    Button btnUpload;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SlikaUpload = (ImageView)findViewById(R.id.SlikaUpload);
+        Uploadime = (EditText)findViewById(R.id.Uploadime);
+        btnUpload = (Button)findViewById(R.id.btnUpload);
+
+        SlikaUpload.setOnClickListener(this);
+        btnUpload.setOnClickListener(this);
+
     }
 
+
     @Override
-    public void onNavigationDrawerItemSelected(int position) {
+    public void onClick(View v) {
 
     }
 }
