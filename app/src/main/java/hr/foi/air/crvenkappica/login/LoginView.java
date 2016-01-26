8a980c0f05a840b246800249e54d8d414c26e1ca
ddrazeno <ddrazeno@foi.hr>
2016-01-27 00:26:40 +0100

    unit testing

---
+++ app/src/main/java/hr/foi/air/crvenkappica/login/LoginView.java
@@ -0,0 +1,10 @@
+package hr.foi.air.crvenkappica.login;
+
+/**
+ * Created by DOMAGOJ on 26-Jan-16.
+ */
+public interface LoginView {
+    String getUserName();
+
+    void showUserNameError(int resId);
+}
